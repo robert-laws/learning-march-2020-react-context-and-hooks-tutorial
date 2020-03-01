2de285b8f9cdc6be75abce9db26327a80f56d9c7
Robert Laws <roblaws@me.com>
2020-03-01 12:43:41 +0300

    beginning setup

---
+++ src/components/BookList.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export const BookList = () => {
+  return (
+    <div className="book-list">
+      <ul>
+        <li>the way of kings</li>
+        <li>the name of the wind</li>
+        <li>the final empire</li>
+      </ul>
+    </div>
+  )
+}
